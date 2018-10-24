cf1f5feef44f16c4f4b0cf5197843d76ffa9bb74
John Hoerr <jhoerr@gmail.com>
2018-10-24 17:53:13 -0400

    Test a real returned data structure

--- functions.tests.integration/Tests.fs
+++ functions.tests.integration/Tests.fs
@@ -1,27 +1,33 @@
-﻿module Tests
-
-open System
-open Xunit
-open SqlServerContainer
-open Chessie.ErrorHandling
+﻿namespace Integration 
+open MyFunctions.Common.Types
 open MyFunctions.Common
 
-// Generally:
-// 1. Go fetch the mssql-server image.
-// 2. Create and start a container.
-// 3. Migrate and populate the DB with test data
-// 4. Exercise DB code
-// 5. Stop and remove the container.
+module Tests=
 
-[<Fact>]
-let ``Get unit from DB`` () = 
-    let container = DateTime.Now.Ticks |> sprintf "mssql-test-%d" 
-    let started = container |> start |> Async.RunSynchronously
-    migrate ()
-    let id = populate () |> Async.RunSynchronously
-    let unit = Database.queryUnit connStr id |> Async.ofAsyncResult |> Async.RunSynchronously
-    unit.ToString() |> Console.WriteLine
-    let stopped = container |> stop |> Async.RunSynchronously
-    container |> delete |> Async.RunSynchronously
-    Assert.True(started)
+    open System
+    open Xunit
+    open SqlServerContainer
+    open Chessie.ErrorHandling
+    open MyFunctions.Common
+
+    // Generally:
+    // 1. Go fetch the mssql-server image.
+    // 2. Create and start a container.
+    // 3. Migrate and populate the DB with test data
+    // 4. Exercise DB code
+    // 5. Stop and remove the container.
+
+    [<Fact>]
+    let ``Get unit from DB`` () = async {
+        // try 
+            // let! started = start ()
+            migrate ()
+            let! id = populate ()
+            let expected = Ok({Fakes.cito with Id=id},[])
+            let! actual = Database.queryUnit connStr id |> Async.ofAsyncResult
+            let actualUnit = lift (fun a -> a.Unit) actual
+            Assert.Equal(expected, actualUnit)
+        // finally
+            // stop () |> ignore
+    }
 
