f83ba9a29e6da19afa0bd2b2e99be1b7f10e273f
John Hoerr <jhoerr@gmail.com>
2018-10-31 09:21:51 -0400

    Tag an IU registry image; don't start the server.

--- docker-start.sh
+++ docker-start.sh
@@ -7,7 +7,8 @@ docker rm funcs
 # build and start
 docker build -t funcs .
 docker tag funcs uitsssl/itpeople-functions:sandbox
-docker run -p 9090:80 -d --name funcs funcs
+docker tag funcs registry-test.docker.iu.edu/repositories/dcd/itpeople-functions:sandbox
+#docker run -p 9090:80 -d --name funcs funcs
 
 # open browser
-open http://localhost:9090/api/ping
+#open http://localhost:9090/api/ping
