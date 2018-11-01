73dba7a64bd3650efb2fac7bfa871e85b186286d
Brent Moberly <bmoberly@iu.edu>
2018-11-01 13:52:12 -0400

    implement basic state server scaffolding

--- functions.tests.stateserver/Functions.fs
+++ functions.tests.stateserver/Functions.fs
@@ -9,22 +9,43 @@ open Microsoft.Extensions.Logging
 open Microsoft.Azure.WebJobs.Host
 open Microsoft.Azure.WebJobs.Host
 open Chessie.ErrorHandling
+open Common.Http
+open System.Net
 
 ///<summary>
 /// This module defines the bindings and triggers for all functions in the project
 ///</summary
 module Functions =
 
-    let setState (reg:HttpRequestMessage) (log:TraceWriter) = 
-        log.Info "hello world" 
-        
+    type ProviderState = {
+        Consumer: string
+        State: string
+    }
+
+    let someFunction () = asyncTrial {
+        return "yay"
+    }
+
+    let matcher (arg:string) = 
+        match arg with 
+        | "test" -> ok someFunction
+        | _ ->  fail (HttpStatusCode.BadRequest, (sprintf "no state defined for %s" arg))
+    
+         
+    let setState (req: HttpRequestMessage) = asyncTrial {
+        let! state = deserializeBody<ProviderState> req
+        let! fn = matcher state.State
+        let! result = fn()
+        return result
+    }
 
     /// (Anonymous) A function that simply returns, "Pong!" 
     [<FunctionName("InitializeState")>]
     let ping
         ([<HttpTrigger(Extensions.Http.AuthorizationLevel.Anonymous, "post", Route = "state")>]
         req: HttpRequestMessage, log: TraceWriter) =
-        
-        Api.Common.getResponse' log setState
+        let fn () = setState req
+        Api.Common.getResponse' log fn
 
+    
     
