3afedde36b71f4b5d90d570fee7a92b8de99f163
Brent Moberly <bmoberly@iu.edu>
2018-11-01 14:18:27 -0400

    implement reset database and state handler for "unit 1 is present"

--- functions.tests.stateserver/Functions.fs
+++ functions.tests.stateserver/Functions.fs
@@ -11,30 +11,47 @@ open Microsoft.Azure.WebJobs.Host
 open Chessie.ErrorHandling
 open Common.Http
 open System.Net
+open Npgsql
+open Dapper
+open MyFunctions.Common.Fakes
+open Migrations.Program
 
 ///<summary>
 /// This module defines the bindings and triggers for all functions in the project
 ///</summary
 module Functions =
 
+    let connStr = "User ID=root;Host=localhost;Port=5432;Database=circle_test;Pooling=true;"
+    
     type ProviderState = {
         Consumer: string
         State: string
     }
 
-    let someFunction () = asyncTrial {
-        return "yay"
+    let resetDatabase () = 
+        try
+            clearAndMigrate connStr
+            ok ()
+        with
+        | exn -> fail (HttpStatusCode.InternalServerError, (sprintf "migration failed with %s" (exn.ToString())))
+    
+    let populateDatabaseWithUnit1 () = asyncTrial {
+        let dbConnection = new NpgsqlConnection(connStr)
+        let! result = dbConnection.InsertAsync<Unit>(cito) |> Async.AwaitTask 
+        return result.GetValueOrDefault()
     }
 
     let matcher (arg:string) = 
         match arg with 
-        | "test" -> ok someFunction
+        | "unit 1 is present" -> ok populateDatabaseWithUnit1
         | _ ->  fail (HttpStatusCode.BadRequest, (sprintf "no state defined for %s" arg))
     
          
     let setState (req: HttpRequestMessage) = asyncTrial {
         let! state = deserializeBody<ProviderState> req
         let! fn = matcher state.State
+
+        do! resetDatabase()
         let! result = fn()
         return result
     }
