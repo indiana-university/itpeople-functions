0645340380ca9a1fba95591b09df93b84bc5f100
John Hoerr <jhoerr@gmail.com>
2018-11-05 12:33:09 -0500

    Cleanup

--- .teamcity/docker-service-update.sh
+++ .teamcity/docker-service-update.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Source the Docker client bundle for the environment associated with this build.
-source $HOME/.dcd/%DockerUcpBundle%.sh
+source $HOME/.dcd/%DockerUcp.Bundle%.sh
 
 # Update all service secrets required by the functions
 docker secret rm itpeople-functions-OAuthClientId
-printf "%Functions.OAuthClientId" | docker secret create itpeople-functions-OAuthClientId -
-docker secret rm itpeople-functions-OAuthClientId
-printf "Functions.%OAuthClientId" | docker secret create itpeople-functions-OAuthClientId -
+printf "%Functions.OAuthClientId%" | docker secret create itpeople-functions-OAuthClientId -
+docker secret rm itpeople-functions-OAuthClientSecret
+printf "Functions.%OAuthClientSecret%" | docker secret create itpeople-functions-OAuthClientSecret -
 docker secret rm itpeople-functions-JwtSecret
-printf "%Functions.JwtSecret" | docker secret create itpeople-functions-JwtSecret -
+printf "%Functions.JwtSecret%" | docker secret create itpeople-functions-JwtSecret -
 docker secret rm itpeople-functions-DbConnectionString
-printf "%Functions.DbConnectionString" | docker secret create itpeople-functions-DbConnectionString -
+printf "%Functions.DbConnectionString%" | docker secret create itpeople-functions-DbConnectionString -
 
 # Update the service and non-secret environment variables
 docker service update \
