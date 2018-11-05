115448de8075f4c4e1264fb5f67a783a02316e6a
John Hoerr <jhoerr@gmail.com>
2018-11-05 12:15:59 -0500

    Namespace the Function app settings

--- .teamcity/docker-service-update.sh
+++ .teamcity/docker-service-update.sh
@@ -5,19 +5,19 @@ source $HOME/.dcd/%DockerUcpBundle%.sh
 
 # Update all service secrets required by the functions
 docker secret rm itpeople-functions-OAuthClientId
-printf "%OAuthClientId" | docker secret create itpeople-functions-OAuthClientId -
+printf "%Functions.OAuthClientId" | docker secret create itpeople-functions-OAuthClientId -
 docker secret rm itpeople-functions-OAuthClientId
-printf "%OAuthClientId" | docker secret create itpeople-functions-OAuthClientId -
+printf "Functions.%OAuthClientId" | docker secret create itpeople-functions-OAuthClientId -
 docker secret rm itpeople-functions-JwtSecret
-printf "%JwtSecret" | docker secret create itpeople-functions-JwtSecret -
+printf "%Functions.JwtSecret" | docker secret create itpeople-functions-JwtSecret -
 docker secret rm itpeople-functions-DbConnectionString
-printf "%DbConnectionString" | docker secret create itpeople-functions-DbConnectionString -
+printf "%Functions.DbConnectionString" | docker secret create itpeople-functions-DbConnectionString -
 
 # Update the service and non-secret environment variables
 docker service update \
-    --env-add OAuthTokenUrl=%OAuthTokenUrl% \
-    --env-add OAuthRedirectUrl=%OAuthRedirectUrl% \
-    --env-add SPA_HOST=%SPA_Host% \
-    --env-add API_HOST=%API_Host% \
+    --env-add OAuthTokenUrl=%Functions.OAuthTokenUrl% \
+    --env-add OAuthRedirectUrl=%Functions.OAuthRedirectUrl% \
+    --env-add SPA_HOST=%Functions.SPA_Host% \
+    --env-add API_HOST=%Functions.API_Host% \
     --image %DockerHub.Repo%:%vcsroot.branch% \
     itpeople-functions 
