6d36ee0db6f48f6ceee0f60408d94e9505ec2c88
John Hoerr <jhoerr@gmail.com>
2018-11-07 17:02:36 -0500

    Logging

--- .teamcity/docker-service-update.sh
+++ .teamcity/docker-service-update.sh
@@ -7,9 +7,10 @@ export DOCKER_TAG=${TEAMCITY_BRANCH/\/refs\/heads\//}
 source $HOME/.dcd/$DOCKER_UCP_BUNDLE.sh
 
 # Update the service and non-secret environment variables
+echo Updating itpeople-functions service from $DOCKER_HUB_REPO:$DOCKER_TAG
 docker service update --image $DOCKER_HUB_REPO:$DOCKER_TAG \
     --health-cmd 'curl --fail localhost:80/api/ping || exit 1' \
-    --health-interval 1s \
+    --health-interval 2s \
     --health-retries 120 \
     --health-start-period 10s \
     --health-timeout 5s \
