caa021ae2e958bc5e06a85450188ee3d8965fdf8
John Hoerr <jhoerr@gmail.com>
2018-11-07 10:41:17 -0500

    Fix docker login complaint and bad env variable

--- .teamcity/docker-build-tag-push.sh
+++ .teamcity/docker-build-tag-push.sh
@@ -2,10 +2,10 @@
 
 # Build the docker image and tag it for docker hub
 docker build -t functions .
-docker tag functions $DOCKER_HUB_REPO:$BRANCH
+docker tag functions $DOCKER_HUB_REPO:$TEAMCITY_BRANCH
 
 # Login to Docker Hub
-docker login --username $DOCKER_HUB_USERNAME --password $DOCKER_HUB_PASSWORD
+printf $DOCKER_HUB_PASSWORD | docker login --username $DOCKER_HUB_USERNAME --password-stdin
 
 # Push the image to Docker Hub
 docker push $DOCKER_HUB_REPO:$BRANCH
