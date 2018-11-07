291d756697cb07493bbe2f57a225187b5ad509aa
John Hoerr <jhoerr@gmail.com>
2018-11-07 16:44:00 -0500

    export the resolved branch name as an env variable

--- .teamcity/docker-build-tag-push.sh
+++ .teamcity/docker-build-tag-push.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Resolve the current branch name
-resolved_branch = ${TEAMCITY_BRANCH/\/refs\/heads\//}
+export RESOLVED_BRANCH=${TEAMCITY_BRANCH/\/refs\/heads\//}
 
 # Build the docker image and tag it for docker hub
 docker build -t functions .
 
 # Tag image for docker hub
-echo Tagging functions image as $DOCKER_HUB_REPO:$resolved_branch
-docker tag functions $DOCKER_HUB_REPO:$resolved_branch
+echo Tagging functions image as $DOCKER_HUB_REPO:$RESOLVED_BRANCH
+docker tag functions $DOCKER_HUB_REPO:$RESOLVED_BRANCH
 
 # Login to Docker Hub
 printf $DOCKER_HUB_PASSWORD | docker login --username $DOCKER_HUB_USERNAME --password-stdin
 
 # Push the image to Docker Hub
-echo Pushing $DOCKER_HUB_REPO:$resolved_branch to Docker Hub
-docker push $DOCKER_HUB_REPO:$resolved_branch
+echo Pushing $DOCKER_HUB_REPO:$RESOLVED_BRANCH to Docker Hub
+docker push $DOCKER_HUB_REPO:$RESOLVED_BRANCH
--- .teamcity/docker-service-update.sh
+++ .teamcity/docker-service-update.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Resolve the current branch name by stripping any leading '/refs/heads/'
-resolved_branch = ${TEAMCITY_BRANCH/\/refs\/heads\//}
-
 # Source the Docker client bundle for the environment associated with this build.
 source $HOME/.dcd/$DOCKER_UCP_BUNDLE.sh
 
 # Update the service and non-secret environment variables
-docker service update --image $DOCKER_HUB_REPO:$resolved_branch \
+docker service update --image $DOCKER_HUB_REPO:$RESOLVED_BRANCH \
     --health-cmd 'curl --fail localhost:80/api/ping || exit 1' \
     --health-interval 1s \
     --health-retries 120 \
