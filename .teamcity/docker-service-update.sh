8d2c86b2433b6a0c793b7f2888ace0cedc91ef64
John Hoerr <jhoerr@gmail.com>
2018-11-08 07:35:53 -0800

    Resolve branch name for Docker tag (#12)

--- .teamcity/docker-service-update.sh
+++ .teamcity/docker-service-update.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# Export the tag name
+export DOCKER_TAG=${TEAMCITY_BRANCH/\/refs\/heads\//}
+
 # Source the Docker client bundle for the environment associated with this build.
 source $HOME/.dcd/$DOCKER_UCP_BUNDLE.sh
 
 # Update the service and non-secret environment variables
-docker service update --image $DOCKER_HUB_REPO:$TEAMCITY_BRANCH \
+echo Updating itpeople-functions service from $DOCKER_HUB_REPO:$DOCKER_TAG
+docker service update --image $DOCKER_HUB_REPO:$DOCKER_TAG \
     --health-cmd 'curl --fail localhost:80/api/ping || exit 1' \
-    --health-interval 1s \
+    --health-interval 2s \
     --health-retries 120 \
     --health-start-period 10s \
     --health-timeout 5s \
