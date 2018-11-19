771d5a852db7ea81a5af9434c1cc7c4c916e066e
John Hoerr <jhoerr@gmail.com>
2018-11-19 08:38:04 -0600

    Serialize option types as null or the unwrapped type

--- functions/Common/Http.fs
+++ functions/Common/Http.fs
@@ -14,6 +14,37 @@ open Newtonsoft.Json.Serialization
 open Microsoft.AspNetCore.WebUtilities
 open Serilog.Core
 
+module Json =
+
+    open System
+    open Microsoft.FSharp.Reflection
+    open Newtonsoft.Json
+    open Newtonsoft.Json.Converters
+
+    type OptionConverter() =
+        inherit JsonConverter()
+
+        override x.CanConvert(t) = 
+            t.IsGenericType && t.GetGenericTypeDefinition() = typedefof<option<_>>
+
+        override x.WriteJson(writer, value, serializer) =
+            let value = 
+                if value = null then null
+                else 
+                    let _,fields = FSharpValue.GetUnionFields(value, value.GetType())
+                    fields.[0]  
+            serializer.Serialize(writer, value)
+
+        override x.ReadJson(reader, t, existingValue, serializer) =        
+            let innerType = t.GetGenericArguments().[0]
+            let innerType = 
+                if innerType.IsValueType then (typedefof<Nullable<_>>).MakeGenericType([|innerType|])
+                else innerType        
+            let value = serializer.Deserialize(reader, innerType)
+            let cases = FSharpType.GetUnionCases(t)
+            if value = null then FSharpValue.MakeUnion(cases.[0], [||])
+            else FSharpValue.MakeUnion(cases.[1], [|value|])
+
 module Http =
 
     let client = new HttpClient()
@@ -50,8 +81,10 @@ module Http =
     /// JSON Serialization Defaults:
     /// 1. Format property names in 'camelCase'.
     /// 2. Convert all enum values to/from their string equivalents.
+    /// 3. Format all options as null or the unwrapped type
     let jsonSettings = JsonSerializerSettings(ContractResolver=CamelCasePropertyNamesContractResolver())
     jsonSettings.Converters.Add(Newtonsoft.Json.Converters.StringEnumConverter())
+    jsonSettings.Converters.Add(Json.OptionConverter())
 
     let addCORSHeader (res:HttpResponseMessage) (origin:string) (corsHosts:string) =
         match corsHosts with
