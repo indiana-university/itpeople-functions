9ffef63b1e6d07a5d893f15088cf4432ad19f2a8
John Hoerr <jhoerr@gmail.com>
2019-02-21 20:57:29 -0500

    Stop using FSharp.Data; reimpl test using built-in HttpClient.
    Added a little fluent API for setting up HTTP requests aginst the functions app.

--- functions.tests.integration/ApiErrorTests.fs
+++ functions.tests.integration/ApiErrorTests.fs
@@ -6,38 +6,41 @@ module ApiErrorTests =
     open Xunit
     open TestFixture
     open Functions.Fakes
-    open Database.Fakes
-    open FSharp.Data
-    open FSharp.Data.HttpRequestHeaders
+    open Functions.Json
     open FsUnit.Xunit
     open Newtonsoft.Json
+    open System.Net
+    open System.Net.Http
+    open System.Net.Http.Headers
 
-    type Body = 
-        | NoBody
-        | Json of string
-        | Record of obj
+    let httpClient = new HttpClient()
 
     type ApiErrorTests(output: ITestOutputHelper)=
         inherit HttpTestBase(output)
 
-        let authdContent = 
-          [ (Authorization "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOiIxOTE1NTQ0NjQzIiwidXNlcl9uYW1lIjoiam9obmRvZSIsInVzZXJfaWQiOjF9.bCMuAfRby19tJHCOggz7KESMRxtPl_h7pLTQTx3ui4E")
-            (ContentType HttpContentTypes.Json) ]
+        let requestFor method route = 
+            let uri = sprintf "%s/%s" functionServerUrl route |> System.Uri
+            new HttpRequestMessage(method, uri)
 
-        let authdRequest method route body = 
-            let body = 
-                match body with
-                | NoBody -> None
-                | Json str -> str |> TextRequest |> Some
-                | Record obj -> JsonConvert.SerializeObject(obj, Functions.Json.JsonSettings) |> TextRequest |> Some
-            match body with
-            | Some (b) -> Http.Request((sprintf "%s/%s" functionServerUrl route), httpMethod=method, body=b, headers=authdContent, silentHttpErrors=true)
-            | None -> Http.Request((sprintf "%s/%s" functionServerUrl route), httpMethod=method, headers=authdContent, silentHttpErrors=true)
+        let withAuthentication (request:HttpRequestMessage) = 
+            request.Headers.Authorization <- AuthenticationHeaderValue("Bearer", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOiIxOTE1NTQ0NjQzIiwidXNlcl9uYW1lIjoiam9obmRvZSIsInVzZXJfaWQiOjF9.bCMuAfRby19tJHCOggz7KESMRxtPl_h7pLTQTx3ui4E")
+            request
+        
+        let withRawBody str (request:HttpRequestMessage) =
+            request.Content <- new StringContent(str, System.Text.Encoding.UTF8, "application/json")
+            request
+
+        let withBody obj (request:HttpRequestMessage) =
+            withRawBody (JsonConvert.SerializeObject(value=obj, settings=JsonSettings)) request
+
+        let shouldGetResponse expectedStatus (request:HttpRequestMessage) =
+            let response = httpClient.SendAsync(request) |> Async.AwaitTask |> Async.RunSynchronously
+            response.StatusCode |> should equal expectedStatus
 
         [<Fact>]
         member __.``Unauthorized request yields 401 Unauthorized`` () = 
-            let response = Http.Request(functionServerUrl+"/units", silentHttpErrors=true)
-            response.StatusCode |> should equal 401
+            requestFor HttpMethod.Get "units" 
+            |> shouldGetResponse HttpStatusCode.Unauthorized
 
         [<Theory>]
         [<InlineDataAttribute("units")>]
@@ -46,68 +49,96 @@ module ApiErrorTests =
         [<InlineDataAttribute("supportRelationships")>]
         [<InlineDataAttribute("people")>]
         member __.``Get non-existent resource yields 404 Not Found`` (resource: string) = 
-            let response = authdRequest "GET" "units/1000" NoBody
-            response.StatusCode |> should equal 404
+            sprintf "units/%s/1000" resource
+            |> requestFor HttpMethod.Get
+            |> withAuthentication
+            |> shouldGetResponse HttpStatusCode.NotFound
 
         [<Theory>]
         [<InlineDataAttribute("units")>]
         [<InlineDataAttribute("memberships")>]
         [<InlineDataAttribute("supportRelationships")>]
         member __.``Delete non-existent resource yields 404 Not Found`` (resource: string) = 
-            let response = authdRequest "DELETE" "units/1000" NoBody
-            response.StatusCode |> should equal 404
+            sprintf "units/%s/1000" resource
+            |> requestFor HttpMethod.Delete
+            |> withAuthentication
+            |> shouldGetResponse HttpStatusCode.NotFound
 
         [<Fact>]       
         member __.``Create resource with missing required field in request body yields 400 Bad Request`` () = 
-            let response = Json("""{"description":"d", "url":"u", "parentId":undefined}""") |> authdRequest "POST" "units"
-            response.StatusCode |> should equal 400
+            requestFor HttpMethod.Post "units"
+            |> withAuthentication
+            |> withRawBody """{"description":"d", "url":"u", "parentId":undefined}"""
+            |> shouldGetResponse HttpStatusCode.BadRequest
 
         [<Fact>]       
         member __.``Create a unit with non existent parent yields 400 Bad Request`` () = 
-            let response = Record({ parksAndRec with ParentId = Some(1000) }) |> authdRequest "POST" "units" 
-            response.StatusCode |> should equal 400
+            requestFor HttpMethod.Post "units"
+            |> withAuthentication
+            |> withBody { parksAndRec with ParentId = Some(1000) }
+            |> shouldGetResponse HttpStatusCode.BadRequest
 
         [<Fact>]       
         member __.``Create a unit with existing unit name yields 409 Conflict`` () = 
-            let response = Record(parksAndRec) |> authdRequest "POST" "units" 
-            response.StatusCode |> should equal 409
+            requestFor HttpMethod.Post "units"
+            |> withAuthentication
+            |> withBody parksAndRec
+            |> shouldGetResponse HttpStatusCode.Conflict
 
         [<Fact>]       
         member __.``Update a unit with circular relationship yields 409 Conflict`` () = 
-            let response = Record({ cityOfPawnee with ParentId=Some(parksAndRec.Id) }) |> authdRequest "PUT" "units/1" 
-            response.StatusCode |> should equal 409
+            requestFor HttpMethod.Put "units/1"
+            |> withAuthentication
+            |> withBody { cityOfPawnee with ParentId=Some(parksAndRec.Id) }
+            |> shouldGetResponse HttpStatusCode.Conflict
 
         [<Fact>]       
         member __.``Create a membership with non existent unit yields 400 Bad Request`` () = 
-            let response = Record({ knopeMembership with UnitId=1000 }) |> authdRequest "POST" "memberships" 
-            response.StatusCode |> should equal 400
+            requestFor HttpMethod.Post "memberships"
+            |> withAuthentication
+            |> withBody { knopeMembership with UnitId=1000 }
+            |> shouldGetResponse HttpStatusCode.BadRequest
 
         [<Fact>]       
         member __.``Create a membership with non existent person yields 400 Bad Request`` () = 
-            let response = Record({ knopeMembership with PersonId=Some(1000) }) |> authdRequest "POST" "memberships" 
-            response.StatusCode |> should equal 400
+            requestFor HttpMethod.Post "memberships"
+            |> withAuthentication
+            |> withBody { knopeMembership with PersonId=Some(1000) }
+            |> shouldGetResponse HttpStatusCode.BadRequest
 
         [<Fact>]       
         member __.``Create a membership that duplicates existing memberships yields 409 Conflict`` () = 
-            let response = Record(knopeMembership) |> authdRequest "POST" "memberships" 
-            response.StatusCode |> should equal 409
+            requestFor HttpMethod.Post "memberships"
+            |> withAuthentication
+            |> withBody knopeMembership
+            |> shouldGetResponse HttpStatusCode.Conflict
 
         [<Fact>]       
         member __.``Update a membership that duplicates existing memberships yields 409 Conflict`` () = 
-            let response = Record(knopeMembership) |> authdRequest "PUT" (sprintf "memberships/%d" swansonMembership.Id) 
-            response.StatusCode |> should equal 409
+            sprintf "memberships/%d" swansonMembership.Id
+            |> requestFor HttpMethod.Put
+            |> withAuthentication
+            |> withBody knopeMembership
+            |> shouldGetResponse HttpStatusCode.Conflict
+
 
         [<Fact>]       
         member __.``Create a support relationship with non existent unit yields 400 Bad Request`` () = 
-            let response = Record({ supportRelationship with UnitId=1000 }) |> authdRequest "POST" "supportRelationships" 
-            response.StatusCode |> should equal 400
+            requestFor HttpMethod.Post "supportRelationships"
+            |> withAuthentication
+            |> withBody { supportRelationship with UnitId=1000 }
+            |> shouldGetResponse HttpStatusCode.BadRequest
 
         [<Fact>]       
         member __.``Create a support relationship with non existent department yields 400 Bad Request`` () = 
-            let response = Record({ supportRelationship with DepartmentId=1000 }) |> authdRequest "POST" "supportRelationships" 
-            response.StatusCode |> should equal 400
+            requestFor HttpMethod.Post "supportRelationships"
+            |> withAuthentication
+            |> withBody { supportRelationship with DepartmentId=1000 }
+            |> shouldGetResponse HttpStatusCode.BadRequest
 
         [<Fact>]       
         member __.``Create a supportRelationship that duplicates existing relationship yields 409 Conflict`` () = 
-            let response = Record(supportRelationship) |> authdRequest "POST" "supportRelationships" 
-            response.StatusCode |> should equal 409
+            requestFor HttpMethod.Post "supportRelationships"
+            |> withAuthentication
+            |> withBody supportRelationship
+            |> shouldGetResponse HttpStatusCode.Conflict
