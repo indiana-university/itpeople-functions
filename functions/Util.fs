6681193660964fac7d81c35f32e7ee28b6cf71c5
John Hoerr <jhoerr@gmail.com>
2019-03-15 16:30:23 -0400

    Not dead yet!

--- functions/Util.fs
+++ functions/Util.fs
@@ -7,6 +7,9 @@ module Util =
 
     open System
 
+    /// An active pattern to identify empty sequences
+    let (|EmptySeq|_|) a = if Seq.isEmpty a then Some () else None
+
     let invariantEqual (str:string) arg = 
         str.Equals(arg, System.StringComparison.InvariantCultureIgnoreCase)
 
