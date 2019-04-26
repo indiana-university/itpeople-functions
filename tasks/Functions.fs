fcf4bc2734d41457859c0daed33a580cc7d792d9
John Hoerr <jhoerr@gmail.com>
2019-04-26 12:45:21 -0400

    Change task namespace; reformat.

--- tasks/Functions.fs
+++ tasks/Functions.fs
@@ -1,7 +1,7 @@
 // Copyright (C) 2018 The Trustees of Indiana University
 // SPDX-License-Identifier: BSD-3-Clause    
 
-namespace ScheduledTasks
+module Tasks.Functions
 
 open Core.Types
 open Core.Util
@@ -11,28 +11,23 @@ open Microsoft.Azure.WebJobs
 open Microsoft.Extensions.Logging
 
 /// This module defines the bindings and triggers for all functions in the project
-module Functions =    
 
-    // ********************
-    // ** Cron Jobs
-    // ********************
+[<FunctionName("CronTest")>]
+let cronTest
+    ([<TimerTrigger("0 */1 * * * *")>] timer: TimerInfo,
+     [<Queue("test-queue")>] queue: ICollector<string>,
+     log: ILogger) =
+    // Log invocation
+    let timestamp = DateTime.Now.ToLongTimeString()
+    sprintf "Timed function fired at %A. " timestamp |> log.LogInformation
+    // Queue a message 
+    let msg = sprintf "queue message @ %s" timestamp
+    sprintf "Enqueued msg: '%s'" msg |> log.LogInformation
+    queue.Add msg
 
-    [<FunctionName("CronTest")>]
-    let cronTest
-        ([<TimerTrigger("0 */1 * * * *")>] timer: TimerInfo,
-         [<Queue("test-queue")>] queue: ICollector<string>,
-         log: ILogger) =
-        // Log invocation
-        let timestamp = DateTime.Now.ToLongTimeString()
-        sprintf "Timed function fired at %A. " timestamp |> log.LogInformation
-        // Queue a message 
-        let msg = sprintf "queue message @ %s" timestamp
-        sprintf "Enqueued msg: '%s'" msg |> log.LogInformation
-        queue.Add msg
-
-    [<FunctionName("QueueTest")>]
-    let queueTest
-        ([<QueueTrigger("test-queue")>] item: string,
-         log: ILogger) =
-        // Log the dequeued message
-        sprintf "Dequeued msg: '%s'" item |> log.LogInformation
+[<FunctionName("QueueTest")>]
+let queueTest
+    ([<QueueTrigger("test-queue")>] item: string,
+     log: ILogger) =
+    // Log the dequeued message
+    sprintf "Dequeued msg: '%s'" item |> log.LogInformation
