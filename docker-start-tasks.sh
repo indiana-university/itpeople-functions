d10bca3d519993abfc56ede8430ccca7c1a2541e
John Hoerr <jhoerr@gmail.com>
2019-04-30 14:18:39 -0400

    Housekeeping

--- docker-start-tasks.sh
+++ docker-start-tasks.sh
@@ -9,7 +9,16 @@ docker rm tasks
 docker build -f Dockerfile.Tasks -t tasks .
 #docker tag funcs uitsssl/itpeople-functions:sandbox
 #docker tag funcs registry-test.docker.iu.edu/repositories/dcd/itpeople-functions:sandbox
-docker run -p 7081:80 -d --name tasks tasks
+# docker run -p 7071:80 -d --name tasks tasks
+
+docker run \
+-e "AzureWebJobsStorage=" \
+-e "AzureWebJobsDashboard=" \
+-e "FUNCTIONS_WORKER_RUNTIME=dotnet" \
+-e "WEBSITE_SITE_NAME=tasks" \
+-e "WEBSITE_INSTANCE_ID=tasks" \
+-p 7071:80 -d \
+--name tasks tasks
 
 # open browser
-open http://localhost:7081/ping
+open http://localhost:7071/api/ping
