723e5d234ff11f66d8af908856c42055ac4cee75
John Hoerr <jhoerr@gmail.com>
2020-03-20 23:26:28 -0400

    Add logging to tools functions; refactor

--- tasks/Tools.fs
+++ tasks/Tools.fs
@@ -9,6 +9,7 @@ module Tools =
     open Microsoft.Azure.WebJobs
     open Microsoft.Extensions.Logging
     open Novell.Directory.Ldap
+    open Logging
 
     type ADPath = string
     type ToolName = string
@@ -116,66 +117,61 @@ module Tools =
             | Remove(netid, path, name) -> { NetId=netid; ToolPath=path; ToolName=name; ChangeType="remove"; }
         execute connStr sql param
 
-    let enqueueTools (log:ILogger) (queue:ICollector<string>) connStr = pipeline {
-        let! tools = getAllTools connStr
+    let enqueueTools (queue:ICollector<string>) connStr (log:Serilog.ILogger) = pipeline {
+        let! tools = getAllTools connStr        
+        log |> logInfo (sprintf "Enqueueing updates for %d tools." (Seq.length tools)) None
+        
         tools 
-        |> Seq.map serialize 
+        |> Seq.map serialize  
         |> Seq.iter queue.Add
-        tools
-        |> Seq.map (fun t -> sprintf "%s: %s" t.Name t.ADPath)
-        |> String.concat "\n"
-        |> sprintf "Enqueued tool permission updates for: %s"
-        |> log.LogInformation
+        
+        let logEnqeued (t:Tool) = log |> logInfo (sprintf "Enqueued update for %s." t.Name) (Some(t))
+        tools |> Seq.iter logEnqeued        
         return ()
     }
 
-    let enqueueAccessUpdates (log:ILogger) (queue:ICollector<string>) toolJson connStr adUser adPassword = pipeline {
-         let fetchNetids (tool:Tool) = async {
-            let! adPromise = getADGroupMembers adUser adPassword tool.ADPath |> Async.StartChild
-            let! dbPromise = getAllToolUsers connStr tool |> Async.StartChild
-            let! ad = adPromise
-            let! db = dbPromise
-            return 
-                match (ad, db) with
-                | (Ok(adr), Ok(dbr)) -> Ok (tool, adr, dbr) 
-                | (Error(ade),_) -> Error(ade)
-                | (_,Error(dbe)) -> Error(dbe)
-         }
+    let private guardAgainstClearingOfGroup (tool:Tool) adGroupMembers addToAD remFromAD = 
+        let countOfMembers = Seq.length adGroupMembers
+        let countOfAdded = Seq.length addToAD
+        let countOfRemoved = Seq.length remFromAD
+        if (countOfAdded = 0 && countOfRemoved <> 0 && countOfRemoved = countOfMembers)
+        then error (Status.InternalServerError, sprintf "All %d tool grants for %s would be removed!" countOfMembers tool.Name)
+        else ok ()
 
-         let generateADActions (tool:Tool, ad:seq<NetId>, db:seq<NetId>) = 
-            let addToAD = 
-                db 
-                |> Seq.except ad 
-                |> Seq.map (fun a -> Add(a, tool.ADPath, tool.Name))
-            let removeFromAD = 
-                ad 
-                |> Seq.except db 
-                |> Seq.map (fun a -> Remove(a, tool.ADPath, tool.Name))                
+    let private enqueueUpdates (queue:ICollector<string>) (tool:Tool) addToAD remFromAD =
+        let adds = addToAD |> Seq.map (fun a -> Add(a, tool.ADPath, tool.Name))
+        let rems = remFromAD |> Seq.map (fun a -> Remove(a, tool.ADPath, tool.Name))
+        adds
+        |> Seq.append rems 
+        |> Seq.map serialize 
+        |> Seq.iter queue.Add
+        Seq.length adds + Seq.length rems
 
-            let countOfMembers = Seq.length ad
-            let countOfAdded = Seq.length addToAD
-            let countOfRemoved = Seq.length removeFromAD
-            if (countOfAdded = 0 && countOfRemoved <> 0 && countOfRemoved = countOfMembers)
-            then error (Status.InternalServerError, sprintf "All %d tool grants for %s would be removed!" countOfMembers tool.Name)
-            else ok (Seq.append addToAD removeFromAD)
-                   
-         sprintf "Processing tool update %s" toolJson |> log.LogInformation
-         let! tool = tryDeserializeAsync<Tool> toolJson
-         let! netids = fetchNetids tool
-         let! actions = generateADActions netids
-         actions |> Seq.map serialize |> Seq.iter queue.Add
-         return ()
+    let enqueueAccessUpdates (queue:ICollector<string>) toolJson connStr adUser adPassword (log:Serilog.ILogger) = pipeline {
+        let! tool = tryDeserializeAsync<Tool> toolJson
+        log |> logInfo (sprintf "Processing tool access update for %s" tool.Name) None
+        let! adGroupMembers = getADGroupMembers adUser adPassword tool.ADPath
+        log |> logInfo (sprintf "Found %d members of tool AD group" (Seq.length adGroupMembers)) None        
+        let! toolUsers = getAllToolUsers connStr tool 
+        log |> logInfo (sprintf "Found %d netids with access to tool" (Seq.length toolUsers)) None
+
+        let addToAD = toolUsers |> Seq.except adGroupMembers |> Seq.sort 
+        log |> logInfo (sprintf "Tool access will be granted to %d netids" (Seq.length addToAD)) (Some(addToAD))        
+        let remFromAD =  adGroupMembers |> Seq.except toolUsers |> Seq.sort                
+        log |> logInfo (sprintf "Tool access will be revoked from %d netids" (Seq.length remFromAD)) (Some(remFromAD))
+
+        do! guardAgainstClearingOfGroup tool adGroupMembers addToAD remFromAD
+
+        let count = enqueueUpdates queue tool addToAD remFromAD
+        log |> logInfo (sprintf "Enqueued %d AD group memberships updates" count) None
+        
+        return ()
     }
 
-    let updatePersonAccess (log:ILogger) updateJson connStr adUser adPassword = pipeline {
-        let logUpdate =
-            sprintf "Updated Tool AD Group: %A"
-            >> log.LogInformation
-         
-        sprintf "Processing tool person update %s" updateJson |> log.LogInformation
+    let updatePersonAccess updateJson connStr adUser adPassword (log:Serilog.ILogger) = pipeline {
         let! update = tryDeserializeAsync<ToolPersonUpdate> updateJson
+        log |> logInfo "Processing AD group update" (Some(update))
         do! logADGroupUpdate connStr update
         do! updateADGroup adUser adPassword update
-        logUpdate update
         return ()         
     }
